9cf5bb5adf5b573dd5abb64e9e3d2ab9221c9fc2
Kyle <thisisnotkylesemail@gmail.com>
2018-11-21 15:27:54 -0600

    a

--- commands/resizegif.js
+++ commands/resizegif.js
@@ -14,20 +14,40 @@ var download = function(uri, filename, callback){
 */
 module.exports = (self) => {
     self.registerCommand('resizegif', function (msg, args) {
-        var gif
         msg.channel.getMessages(2).then(msgs => {
-            gif = msgs[1].attachments[0] ? msgs[1].attachments[0] : msg.attachments[0]
+            var gif = ""
+             switch(true){
+                case msg.attachments[0] != undefined:
+                    gif = msg.attachments[0]
+                    break;
+                case (msg.embeds[0] != undefined) && msg.embeds[0].type == "image":
+                    gif = msg.embeds[0]
+                    gif.filename = gif.url.split('/')[gif.url.split('/').length-1]
+                    break;
+                case msgs[1].attachments[0] != undefined:
+                    gif = msgs[1].attachments[0]
+                    break;
+                case (msgs[1].embeds[0] != undefined) && msgs[1].embeds[0].type == "image":
+                    gif = msgs[1].embeds[0]
+                    gif.filename = gif.url.split('/')[gif.url.split('/').length-1]
+                    break;
+                default:
+                    return self.createMessage(msg.channel.id, self.config.prefix + ':thumbsdown: Bad gif')
+            }
 
-            console.log(gif)
-            console.log(gif.url)
-            console.log(gif.height >= 129 )
-    
+            if (!gif.url.endsWith('.gif'))
+                return self.createMessage(msg.channel.id, self.config.prefix + ':thumbsdown: Bad url')
+
             download(gif.url, "./temp/"+gif.filename, function(){
-                exec(`gifsicle.exe --resize-height 129 ${gif.filename} --colors 256 -o ./temp/${gif.filename}`, (err, stdout, stderr) => {
+                exec(`gifsicle.exe --resize-height 129 ./temp/${gif.filename} --colors 256 -o ./temp/${gif.filename}`, (err, stdout, stderr) => {
                     if (err) {
-                        // node couldn't execute the command
+                        console.log(err)
                         return;
                     }
+                    self.createMessage(msg.channel.id, self.config.prefix + ':ok_hand:', {
+                        file: fs.readFileSync(`./temp/${gif.filename}`),
+                        name: gif.filename
+                    })
                 });
             });
         })
