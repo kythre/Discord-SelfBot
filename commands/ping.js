7b04d5cccba486f66382db5817bf48fae527393f
Kyle <thisisnotkylesemail@gmail.com>
2018-11-24 02:04:04 -0600

    upated ping command
    looks better and deletes the ping after before the pong

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,8 @@
 */
 module.exports = (self) => {
   self.registerCommand('ping', function (msg, args) {
-    this.self.createMessage(msg.channel.id, 'Pong!').then(m => this.edit(m, `${m.content} (${m.timestamp - msg.timestamp}ms)`))
+    msg.delete()
+    this.self.createMessage(msg.channel.id, 'Pong!').then(m => this.edit(m, `${m.content} (\`${m.timestamp - msg.timestamp} ms\`)`))
   }, {
     aliases: ['pong']
   })
