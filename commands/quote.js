9a4eb61de47ec99458722e0020f2a026de6c39c3
Kyle <thisisnotkylesemail@gmail.com>
2018-11-24 02:05:19 -0600

    updated quote command
    added ability to quote DMs

--- commands/quote.js
+++ commands/quote.js
@@ -1,22 +1,38 @@
 /*
-  Quote. Quotes a message. Args: 1: messageID 2: channelID (if quoting from a different channel)
+  Quote. Quotes a message. Args: 1: messageID 2: channelID (if quoting from a different channel) 2: dm if the channelID is a DM
 */
 
-module.exports = (self) => {
-  self.registerCommand('quote', function (msg, args) {
-    self.getMessages(args[1] ? args[1] :msg.channel.id, 1, null, null,args[0].toString()).then(msgs=>{
-      this.embed(msg, {
-        color: this.defaultColor,
-        author: {
-            name: msgs[0].author.username,
-            icon_url: msgs[0].author.avatarURL
-        },
-        description: msgs[0].content,
-        footer:{
-            text: msgs[0].channel.name
-        },
-        timestamp: new Date(msgs[0].timestamp).toISOString()
-      })
+module.exports = (self) => {  
+  function quote(that, msg, quotemsg){
+    that.embed(msg, {
+      color: that.defaultColor,
+      author: {
+          name: quotemsg.author.username,
+          icon_url: quotemsg.author.avatarURL
+      },
+      description: quotemsg.content,
+      footer:{
+          text: `${quotemsg.channel.name ? quotemsg.channel.name : quotemsg.author.username} in ${quotemsg.channel.name ? quotemsg.channel.name : 'DMs'}`
+      },
+      image: {
+        url: quotemsg.attachments[0] ? quotemsg.attachments[0].url : ""
+      },
+      timestamp: new Date(quotemsg.timestamp).toISOString()
     })
+  }
+
+  self.registerCommand('quote', function (msg, args) {
+    if (args[2]) {
+      self.getDMChannel(args[1]).then(c => {
+        c.getMessages(1, null, null,args[0].toString()).then(msgs=>{
+          quote(this, msg, msgs[0])
+        })
+      })
+    }else{
+      self.getMessages(args[1] ? args[1] :msg.channel.id, 1, null, null,args[0].toString()).then(msgs=>{
+        quote(this, msg, msgs[0])
+      })
+    }
   })
 }
+
